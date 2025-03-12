ff2b842fb16b0c93e0ea00b4d4b3dda331e0c00c
Duncan Leggat <ccaedle@ucl.ac.uk>
2025-03-12 15:49:28 +0000

    Added in extra plots and made it more easily customised, hopefully?

--- diffusion/diffusion_plots.py
+++ diffusion/diffusion_plots.py
@@ -1,5 +1,8 @@
+#from tqdm.notebook import tqdm
 
 from functools import partial
+
+import math
 
 import iris
 import iris.coord_systems
@@ -9,19 +12,27 @@ import iris.quickplot as qplt
 import matplotlib.colors as mcol
 import matplotlib.patheffects as PathEffects
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.use('Agg')
 import numpy as np
-
 
 def plot_dtheta_trace(cube,coords,label,n_timesteps):
 
+    #   print(cube)
+    print(label)
+    print(cube.coords("latitude"))
+    print(cube.coords("longitude"))
+
     cube_single_node = cube.extract(iris.Constraint(**coords))
+
+#    print(cube_single_node)
 
     print(cube_single_node.coords("latitude"))
     print(cube_single_node.coords("longitude"))
     
     tslice = slice(-n_timesteps,None,None)
     
-    print(cube_single_node)
+ #   print(cube_single_node)
 
 #    print(cube_single_node.data[tslice,...].T)
 
@@ -39,7 +50,7 @@ def plot_dtheta_trace(cube,coords,label,n_timesteps):
     _p0 = ax.pcolormesh(
         t_points,
         z_points,
-        cube_single_node.data[tslice,...].T[1]
+        cube_single_node.data[tslice,...].T
     )
 
     ax.set_title(f"{label}")
@@ -52,22 +63,23 @@ def plot_dtheta_trace(cube,coords,label,n_timesteps):
         ]
     )
     ax.set_xticklabels(xticks - n_timesteps)
-
-    cbar = fig.colorbar(_p0, ax=ax)
     
+    cbar = fig.colorbar(_p0, ax=ax)
+
     plt.savefig(f"{label}.png")
     
-    plt.show()
+#    plt.show()
+
+def make_scatter_plot(cube,timestep,layers, plot_label):
 
 
-def make_scatter_plot(cube,timestep,layers):
-
+    print(plot_label)
     fig,ax = plt.subplots()
 
     cube_single_level = cube.extract(iris.Constraint(**layers))
 
-    print(cube_single_level)
-    
+#    print(cube_single_level)
+
     time_step_cube = cube_single_level[timestep,...]
     lons = time_step_cube.coord("longitude").points
     lats = time_step_cube.coord("latitude").points
@@ -80,24 +92,130 @@ def make_scatter_plot(cube,timestep,layers):
     )
 
     cbar = fig.colorbar(_p0, ax=ax)
-    ax.set_title(f"Name")
+
+    ax.set_title(f"{plot_label}")
     ax.set_xlabel("Longitude")
     ax.set_ylabel("Latitude")
-    
-    plt.show()
-    
-    
-    
-def make_mosaic(locations,cubes_of_interest):
 
-    fig = plt.figure(figsize=(20, 9), constrained_layout=True)
-    axd = fig.subplot_mosaic(
-        [
-            "f",
-        ]
-    )
+    plt.savefig(f"{plot_label}.png")
+
+
+def make_line_plot_one_point(cube,location,layers,plot_label):
+
+    print(f"Line plot {plot_label}")
     
-def main(in_cube):
+    fig,ax = plt.subplots()
+    cube_slimmed = cube.extract(iris.Constraint(**location,**layers))
+
+    print(cube_slimmed.data.T)
+
+    plt.plot(cube_slimmed.data.T)
+
+    ax.set_xlabel("Timestep")
+    ax.set_title(plot_label)
+
+    plt.savefig(f"{plot_label}_time_plot.png")
+
+    ax.set_yscale('log')
+
+    plt.savefig(f"{plot_label}_time_plot_log.png")
+
+def make_multiline_plot(cube, locations, layers, plot_label):
+
+    print(f"Making multi-line {plot_label}")
+
+    fig,ax = plt.subplots()
+
+    max = 1.
+    min = 0.
+
+    for location_name,location in locations.items():
+        print(location_name)
+        cube_slimmed = cube.extract(iris.Constraint(**location,**layers))
+        plt.plot(cube_slimmed.data.T, label=location_name)
+
+
+    ax.set_title(plot_label)
+    ax.set_xlabel("Timestep")
+    plt.legend()
+    plt.savefig(f"{plot_label}_overlayed_plot.png")
+
+def calculate_temp_cube(exner,potent,layer):
+    layer_exner = exner.extract(iris.Constraint(**{"half_levels":layer}))
+    layer_potent_up = potent.extract(iris.Constraint(**{"full_levels":layer+0.5}))
+    layer_potent_down = potent.extract(iris.Constraint(**{"full_levels":layer-0.5}))
+
+    layer_potent = (layer_potent_up + layer_potent_down)/2.
+
+    temp = layer_potent * layer_exner
+
+    return temp
+
+
+def within_limits(number,max,min):
+    if number < max and number > min: return True
+    return False
+    
+def main(in_cube, max_timestep, locations, cubes_of_interest):
+
+ 
+    print(cubes_of_interest)
+
+    # Make the trace plots
+    for loc_label, coords in locations.items():
+        for cube_label, cube in cubes_of_interest.items():
+            plot_label = f"{cube_label}-{loc_label}"
+            plot_dtheta_trace(cube,coords,plot_label,100)
+
+    # Make some scatter plots.
+    levels_of_interest = {
+        "dtheta_slow": ["full_levels",[66,65,64,63,60,50]],
+        "eastward_wind": ["half_levels", [65.5,50.5,52.5,55.5,49.5]],
+    }
+
+    # The first one is timesteps leading up to crash, the second is from the beginning
+    timesteps_of_interest = [0,20,50,100,200,300,1000]
+#    timesteps_of_interest = [max_timestep,max_timestep-20,max_timestep-50,max_timestep-100,max_timestep-200,max_timestep-500]
+    for timestep in timesteps_of_interest:
+        for cube_name, level_deets in levels_of_interest.items():
+            for level in level_deets[1]:
+                plot_label = f"{cube_name}-level{math.ceil(level):02d}-{timestep:04d}_steps_before_crash"
+                plot_label = f"{cube_name}-level{math.ceil(level):02d}-{max_timestep-timestep:04d}_steps_from_zero"
+                make_scatter_plot(cubes_of_interest[cube_name],max_timestep-timestep,{level_deets[0]:level},plot_label)
+
+    # Set to the levels you want the plots for
+    level_of_interest = {
+        "dtheta_slow": ["full_levels",[66,50]],
+        "potential_temp": ["full_levels",[66,50]],
+        "exner_pressure": ["half_levels",[65.5,49.5]],
+        }
+
+    cubes_of_interest["potential_temp"] = cubes.extract_cube("air_potential_temperature")
+    for loc_name,location in locations.items():
+        for cube_name, level_deets in level_of_interest.items():
+            for level in level_deets[1]:
+                plot_label = f"{cube_name}-level{math.ceil(level):02d}-{loc_name}"
+                make_line_plot_one_point(cubes_of_interest[cube_name],location,{level_deets[0]:level},plot_label)
+
+    # layered plots for level of interest plots
+    for cube_name, level_deets in level_of_interest.items():
+        for level in level_deets[1]:
+            plot_label = f"{cube_name}-level{math.ceil(level):02d}"
+            make_multiline_plot(cubes_of_interest[cube_name],locations,{level_deets[0]:level},plot_label)
+        
+    exner = cubes.extract_cube("exner_pressure")
+    potent = cubes.extract_cube("air_potential_temperature")
+
+    # Makes real temp plots for the following layers
+    real_temp_levels = [65.5, 49.5]
+    for real_temp_level in real_temp_levels:
+        temp = calculate_temp_cube(exner,potent,real_temp_level)
+        plot_label = f"real-temp_level{math.ceil(real_temp_level):02d}"
+        print(temp)
+        make_multiline_plot(temp,locations,{"half_levels":real_temp_level},plot_label)
+
+if __name__ == "__main__":
+    input_cube = "lfric_diag.nc"
     cubes = iris.load(in_cube)
 
     print (cubes)
@@ -105,19 +223,27 @@ def main(in_cube):
     cubes_of_interest = {
         "dtheta_slow": cubes.extract_cube("potential_temperature_increment_from_slow_physics"),
         "eastward_wind": cubes.extract_cube("eastward_wind"),
+        "exner_pressure": cubes.extract_cube("exner_pressure"),
+    }
+
+    # Set the timestep before the crash, here!
+    #    max_timestep = 2007 #run57
+    max_timestep = 3132 # run53
+    #    max_timestep = 2031 #run54
+
+    # Locations
+    locations = {
+#        "crash":{"longitude":partial(within_limits,max=149.,min=148.),"latitude":partial(within_limits,max=11.5,min=10.5)}, #run54 - temp on, wind off
+#        "crash":{"longitude":partial(within_limits,max=164.,min=163.),"latitude":partial(within_limits,max=9.,min=8.5)}, #run53 - temp on, wind on
+#        "crash_neg":{"longitude":partial(within_limits,max=164.,min=163.),"latitude":partial(within_limits,max=-5.,min=-5.4)}, #run53
+#        "crash":{"longitude":partial(within_limits,max=144.5,min=144.),"latitude":partial(within_limits,max=-7,min=-8)}, #run57 - temp off, wind off
+#        "crash":{"longitude":partial(within_limits,max=167.,min=166.),"latitude":partial(within_limits,max=2.,min=1.8)}, #run61 - temp off, wind on
+        "ssp":{"longitude":partial(within_limits,max=0.,min=-3.),"latitude":partial(within_limits,max=0.,min=-3.)},
+#        "random":{"longitude":partial(within_limits,max=-19,min=-21),"latitude":partial(within_limits,max=-19,min=-21)}, # commenting because too similar
+        "asp":{"longitude": partial(within_limits, max=180., min=178.), "latitude": partial(within_limits, max=0., min=-3.)},
+        "terminator":{"longitude": partial(within_limits, max=92., min=89.), "latitude": partial(within_limits, max=0., min=-3.)},
     }
     
-    locations = {
-        "crash":{"longitude":144.374,"latitude":-7.646},
-        "ssp":{"longitude":0.,"latitude":0.},
-        "random":{"longitude":20.,"latitude":-20.},
-    }
-    #    for loc_label, coords in locations.items():
-    #        for cube_label, cube in cubes_of_interest.items():
-    #            plot_dtheta_trace(cube,coords,f"{cube_label}-{loc_label}",6)
-    layers = {"full_levels":66}
-    make_scatter_plot(cubes_of_interest["dtheta_slow"],3,layers)
+    main(input_cube, max_timestep, locations, cubes_of_interest)
 
-if __name__ == "__main__":
-    input_cube = "lfric_diag.nc"
-    main(input_cube)
+    
--- diffusion/generate_slides.py
+++ diffusion/generate_slides.py
@@ -16,7 +16,7 @@ def begin_slide(out_file):
 def end_slide(out_file):
     out_file.write("\\end{frame}\n")
     
-def write_single_slide_time_evo(out_file,cube_name,layer,timesteps):
+def write_single_slide_time_evo(out_file,cube_name,layer,timesteps,postfix):
     begin_slide(out_file)
     cube_name_title = "-".join(cube_name.split("_"))
     out_file.write(f"\\frametitle{{{cube_name_title} at layer {layer}}}\n")
@@ -26,7 +26,7 @@ def write_single_slide_time_evo(out_file,cube_name,layer,timesteps):
         end_line = ""
         if plot_n%3 == 0: end_line = "\\\\"
         #        out_file.write(f"\\includegraphics[width=0.32\\textwidth]{{{cube_name}-level{layer}-{timesteps:04d}_steps_before_crash.png}} {end_line}\n")
-        out_file.write(f"\\includegraphics[width=0.32\\textwidth]{{{cube_name}-level{layer}-{timestep:04d}_steps_before_crash.png}} {end_line}\n")
+        out_file.write(f"\\includegraphics[width=0.32\\textwidth]{{{cube_name}-level{layer}-{timestep:04d}{postfix}.png}} {end_line}\n")
     end_slide(out_file)
         
 def main():
@@ -36,14 +36,18 @@ def main():
         "dtheta_slow": [50,60,63,64,65,66],
     }
     timesteps = [0,20,50,100,200,300,1000]
+#    timesteps = [0,20,50,100,200,500]
 
+    postfix = "_steps_before_crash"
+#    postfix = "_steps_from_zero"
+    
     with open("map_presentation.tex","w") as f:
         
         write_opening_slides(f)
         
         for cube in cubes:
             for layer in cubes[cube]:
-                write_single_slide_time_evo(f,cube,layer,timesteps)
+                write_single_slide_time_evo(f,cube,layer,timesteps,postfix)
         close_slides(f)
     
 if __name__ == "__main__":
